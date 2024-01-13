40e8518972cede76ba0a591b7c76605c74194e62
SaraTullini <sara.tullini@edu.unife.it>
2024-01-13 14:07:38 +0100

    modifica app

--- app.py
+++ app.py
@@ -10,13 +10,13 @@
 
 # if __name__ == "__main__":
 #     main()
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'ok, ora forse funzioni'
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
