5396e0bff476482ec6e0bc2335969b119c06f330
SaraTullini <sara.tullini@edu.unife.it>
2024-01-13 14:59:28 +0100

    ricreazione app

--- app.py
+++ app.py
@@ -1,15 +1,3 @@
-# import sys
-
-# def main():
-#     if len(sys.argv) != 2:
-#         print("Usage: python app.py <name>")
-#         sys.exit(1)
-
-#     name = sys.argv[1]
-#     print(f"Hello, {name}!")
-
-# if __name__ == "__main__":
-#     main()
 from flask import Flask
 
 app = Flask(__name__)
